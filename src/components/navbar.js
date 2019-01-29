37a36bd4eb546e691c5c57b0073e7dacf15f5397
ralph44 <lennard.gerdes@gmx.net>
2019-01-29 22:40:31 +0100

    navbar progress

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,67 +1,49 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import {
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+import { Link } from 'gatsby'
 
-const Navbar = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `red`,
-      opacity: `0.5`,
-      padding: `1.45rem 1.0875rem`,
-      marginBottom: `1.45rem`,
-      marginTop: `0`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
-      >
-      <a style={{ }}>
-        <Link
-          to="/page-2/"
-          style={{
+const Navbar1 = ({ siteTitle }) => (
+  <div   
+  style={{
+    background: `red`,
+    padding: `1.45rem 1.0875rem`,
+    marginBottom: `1.45rem`,
+    marginTop: `0`,
+  }}>
+  <Nav pills>
+    <NavItem>
+      <NavLink href="/page-2/" active>
+        <Link to="/page-2/"  style={{
             color: `white`,
             textDecoration: `none`,
-          }}
-        >
-            home
+        }}>
+          Link to Page
         </Link>
-      </a>
-      <a style={{marginLeft: `3rem`}}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          about me
-        </Link>
-      </a>
-      <a style={{marginLeft: `3rem`}}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-            projects
-        </Link>
-      </a>
-    </div>
-  </div>
+      </NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink href="#">Link</NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink href="#">Another Link</NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink disabled href="#">Disabled Link</NavLink>
+    </NavItem>
+  </Nav>
+</div>
 )
 
-Navbar.propTypes = {
+Navbar1.propTypes = {
   siteTitle: PropTypes.string,
 }
 
-Navbar.defaultProps = {
+Navbar1.defaultProps = {
   siteTitle: ``,
 }
 
-export default Navbar
+export default Navbar1
